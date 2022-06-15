69c370bba9b3a1155a747e71dafd65926af76b5e
Aryan181 <62005619+Aryan181@users.noreply.github.com>
2022-06-15 17:40:35 -0400

    changed time config output for better readability in terms of hours and dates

--- app/src/main/java/com/example/mentalhealthco/MainActivity.java
+++ app/src/main/java/com/example/mentalhealthco/MainActivity.java
@@ -15,6 +15,7 @@ import android.util.Log;
 
 import android.app.usage.UsageStatsManager;
 
+import java.util.Date;
 import java.util.List;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -54,35 +55,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getTopActivtyFromLolipopOnwards() {
-
         String topPackageName;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             UsageStatsManager mUsageStatsManager = (UsageStatsManager) getSystemService(Context.USAGE_STATS_SERVICE);
             long time = System.currentTimeMillis();
-
             Log.e(TAG, ""+time);
             // We get usage stats for the last 1000 seconds
             List<UsageStats> stats = mUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 100 * 10, time);
             // Sort the stats by the last time used
-
             if (stats != null) {
                 SortedMap < Long, UsageStats > mySortedMap = new TreeMap < Long, UsageStats > ();
                 for (UsageStats usageStats: stats) {
                     mySortedMap.put(usageStats.getLastTimeUsed(), usageStats);
                 }
-
-
-
                 for(Long val : mySortedMap.keySet()){
-                    Log.e(TAG, ""+mySortedMap.get(val).getPackageName());
+
+                    Date d1 = new Date(mySortedMap.get(val).getFirstTimeStamp());
+                    Date d2 = new Date(mySortedMap.get(val).getLastTimeStamp());
+
+                    Log.e(TAG, ""+mySortedMap.get(val).getPackageName()+ " "+ d1+ " @@ "+ d2);
                 }
-
-
                 if (mySortedMap != null && !mySortedMap.isEmpty()) {
                     topPackageName = mySortedMap.get(mySortedMap.lastKey()).getPackageName();
                     Log.e("TopPackage Name", topPackageName);
                 }
             }
+// start of the day <-> current time of the day
+
+
+
+
+
 
         }
     }
